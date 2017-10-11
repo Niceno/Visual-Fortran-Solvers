102e632ceaf50e8378bddb1508b46f632f5c907e
Bojan Niceno <bojan@niceno.ch>
2017-10-11 09:00:30 +0200

    Gaussian elimination works for a whole sytem :-)

--- Driver.f90
+++ Driver.f90
@@ -10,20 +10,25 @@
   include "Cholesky_Factorization.int"
   include "Gaussian_Elimination.int"
   include "Matrix_Matrix_Multiply.int"
+  include "Matrix_Vector_Multiply.int"
   include "Transpose_Matrix.int" 
 !----------------------------------------------------------------------!
-  integer :: row, col
+  integer :: row, col, choice
 !----------------------------------------------------------------------!
 
 !-----------------------------------+
 ! Matrix 1 for Gaussian elimination !
 !-----------------------------------!
   integer, parameter      :: n1 = 4
-  real, dimension(n1, n1) :: a1
-  data (a1(1,col), col=1,n1) /  6.0, -2.0,  2.0,  4.0 /  ! =--> row 1
-  data (a1(2,col), col=1,n1) / 12.0, -8.0,  6.0, 10.0 /  ! =--> row 2
-  data (a1(3,col), col=1,n1) /  3.0,-13.0,  9.0,  3.0 /  ! =--> row 3
-  data (a1(4,col), col=1,n1) / -6.0,  4.0,  1.0,-18.0 /  ! =--> row 4
+  real, dimension(n1, n1) :: a1, g1
+  real, dimension(n1)     :: b1, x1, y1
+  data (a1(1,col), col=1,n1) /  1.0,  2.0,  1.0, -1.0 /  ! =--> row 1
+  data (a1(2,col), col=1,n1) /  3.0,  2.0,  4.0,  4.0 /  ! =--> row 2
+  data (a1(3,col), col=1,n1) /  4.0,  4.0,  3.0,  4.0 /  ! =--> row 3
+  data (a1(4,col), col=1,n1) /  2.0,  0.0,  1.0,  5.0 /  ! =--> row 4
+  data (b1(row),   row=1,n1) /  5.0, 16.0, 22.0, 15.0 /
+  data (x1(row),   row=1,n1) /  0.0,  0.0,  0.0,  0.0 /
+  data (y1(row),   row=1,n1) /  0.0,  0.0,  0.0,  0.0 /
 
 !-------------------------------------+
 ! Matrix 2 for Cholesky factorization !
@@ -57,54 +62,85 @@
   data (b4(row),   row=1,n4) / 7.0, 5.0, 6.0 /
 !----------------------------------------------------------------------!
 
+  write(*,*) '============================'
+  write(*,*) 'Select a case to demonstrate'
+  write(*,*) '1 - Gaussian elimination'
+  write(*,*) '2 - Cholesky factorization'
+  write(*,*) '3 - Backward substitution'
+  write(*,*) '4 - Forward substitution'
+  write(*,*) '----------------------------'
+  read(*,*) choice
+
   !-------------------------------------!
   !   Demonstrate Gussian elimination   !
   !-------------------------------------!
-  write(*,*) "Original matrix a1:"
-  call Print_Matrix(a1)
+  if(choice == 1) then
+    write(*,*) "Original matrix a1:"
+    call Print_Matrix(a1)
 
-  call Gaussian_Elimination(a1)
+    call Gaussian_Elimination(g1, b1, a1)
 
-  write(*,*) "Matrix a1 after elimination:"
-  call Print_Matrix(a1)
+    write(*,*) "Matrix g1 after elimination:"
+    call Print_Matrix(a1)
+    call Print_Matrix(g1)
+
+    write(*,*) "Source term vector b1 after elimination:"
+    call Print_Vector(b1)
+
+    call Backward_Substitution(x1, g1, b1)
+
+    write(*,*) "Solution vector x1:"
+    call Print_Vector(x1)
+
+    call Matrix_Vector_Multiply(y1, a1, x1)
+
+    write(*,*) "Vector y1 should recover the source term vector:"
+    call Print_Vector(y1)
+  end if
 
   !----------------------------------------!
   !   Demonstrate Cholesky Factorization   !
   !----------------------------------------!
+  if(choice == 2) then
   l2 = 0
-  call Cholesky_Factorization(l2, a2)
+    call Cholesky_Factorization(l2, a2)
 
-  write(*,*) "Matrix l2 after Cholesky factorization:"
-  call Print_Matrix(l2)
+    write(*,*) "Matrix l2 after Cholesky factorization:"
+    call Print_Matrix(l2)
 
-  call Transpose_Matrix(u2, l2)
+    call Transpose_Matrix(u2, l2)
 
-  write(*,*) "Matrix u2 after inversion:"
-  call Print_Matrix(u2)
+    write(*,*) "Matrix u2 after inversion:"
+    call Print_Matrix(u2)
 
-  call Matrix_Matrix_Multiply(p2, l2, u2)
+    call Matrix_Matrix_Multiply(p2, l2, u2)
 
-  write(*,*) "Matrix p2 after multiplication:"
-  call Print_Matrix(p2)
+    write(*,*) "Matrix p2 after multiplication:"
+    call Print_Matrix(p2)
+  end if
 
   !---------------------------------------!
   !   Demonstrate Backward Substitution   !
   !---------------------------------------!
-  write(*,*) "Matrix a3"                          
-  call Print_Matrix(a3)
+  if(choice == 3) then
+    write(*,*) "Matrix a3"                          
+    call Print_Matrix(a3)
 
-  call Forward_Substitution(y3, a3, b3)
+    call Forward_Substitution(y3, a3, b3)
 
-  call Print_Vector(y3) 
+    call Print_Vector(y3) 
+  end if
  
   !--------------------------------------!
   !   Demonstrate Forward Substitution   !
   !--------------------------------------!
-  write(*,*) "Matrix a4"                          
-  call Print_Matrix(a4)
+  if(choice == 4) then
+    write(*,*) "Matrix a4"                          
+    call Print_Matrix(a4)
 
-  call Backward_Substitution(y4, a4, b4)
+    call Backward_Substitution(y4, a4, b4)
 
-  call Print_Vector(y4) 
+    call Print_Vector(y4) 
+  end if
  
   end program Driver
--- Gaussian_Elimination.f90
+++ Gaussian_Elimination.f90
@@ -1,23 +1,34 @@
 !======================================================================!
-  subroutine Gaussian_Elimination(a)
+  subroutine Gaussian_Elimination(g, b, a)
 !----------------------------------------------------------------------!
   implicit none
 !----------------------------------------------------------------------!
+  real, dimension(:,:) :: g
+  real, dimension(:)   :: b
   real, dimension(:,:) :: a
 !----------------------------------------------------------------------!
   integer :: i, j, k, n
-  real    :: xmult
+  real    :: mult
 !----------------------------------------------------------------------!
 
   n = size(a,1)  ! some checks would be possible
 
+  ! Copy the matrix first
+  do i=1,n
+    do j=1,n
+      g(i,j) = a(i,j)
+    end do
+  end do
+
+  ! Make elimination for resulting matrix
   do k=1,n-1
     do i=k+1,n      
-      xmult = a(i,k)/a(k,k)       
-      a(i,k) = 0.0
+      mult = g(i,k)/g(k,k)       
+      g(i,k) = 0.0
       do j=k+1,n    
-        a(i,j) = a(i,j) - xmult*a(k,j)      
+        g(i,j) = g(i,j) - mult*g(k,j)      
       end do
+      b(i) = b(i) - mult*b(k)
     end do
   end do
 
