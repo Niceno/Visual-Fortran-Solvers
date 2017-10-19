29edca15f35e9bca62703a4fa86028222e00ba1b
Niceno <bojan@niceno.ch>
2017-10-20 00:56:43 +0200

    Removed unused parameter.

--- Prec_Form.f90
+++ Prec_Form.f90
@@ -1,5 +1,5 @@
 !==============================================================================!
-  subroutine Prec_Form(N, A, D, prec) 
+  subroutine Prec_Form(N, A, D) 
 !------------------------------------------------------------------------------!
 ! Forms preconditioning matrix "D" from provided matrix "A".                   !
 !------------------------------------------------------------------------------!
@@ -11,40 +11,18 @@
   integer      :: N
   type(Matrix) :: A
   type(Matrix) :: D    
-  integer      :: prec
 !-----------------------------------[Locals]-----------------------------------!
   real     :: sum1
   integer  :: i, j, k
 !==============================================================================!
                  
-  !---------------------------------! 
-  !   1) diagonal preconditioning   !
-  !---------------------------------!
-  if(prec == 1) then        
-    do i=1,N                     
-      D % val(D % dia(i)) = A % val(A % dia(i))           
-    end do                      
-
-  !--------------------------------------------! 
-  !   2) incomplete cholesky preconditioning   !
-  !--------------------------------------------!
-  else if(prec == 2) then   
-    do i = 1,N
-      sum1 = A % val(A % dia(i))       ! take diaginal entry   
-      do j = A % row(i), A % dia(i)-1  ! only lower traingular
-        k = A % col(j)                    
-        sum1 = sum1 - D % val(D % dia(k)) * A % val(j) * A % val(j)  
-      end do
-      D % val(D % dia(i)) = 1.0 / sum1
+  do i = 1,N
+    sum1 = A % val(A % dia(i))       ! take diaginal entry   
+    do j = A % row(i), A % dia(i)-1  ! only lower traingular
+      k = A % col(j)                    
+      sum1 = sum1 - D % val(D % dia(k)) * A % val(j) * A % val(j)  
     end do
-
-  !---------------------------!
-  !   .) no preconditioning   !
-  !---------------------------!
-  else                          
-    do i=1,N
-      D % val(D % dia(i)) = 1.0
-    end do
-  end if 
+    D % val(D % dia(i)) = 1.0 / sum1
+  end do
 
   end subroutine Prec_Form
--- Prec_Solve.f90
+++ Prec_Solve.f90
@@ -1,15 +1,7 @@
 !==============================================================================!
-  subroutine Prec_Solve(N, NB, A, D, x, b, prec) 
+  subroutine Prec_Solve(N, NB, A, D, x, b) 
 !------------------------------------------------------------------------------!
 ! Solves the preconditioning system [D]{x}={b}                                 !
-!------------------------------------------------------------------------------!
-!   Allows preconditioning of the system by:                                   !
-!     1. Diagonal preconditioning                                              !
-!     2. Incomplete Cholesky preconditioning                                   !
-!                                                                              !
-!   The type of precondtioning is chosen by setting the variable prec to 0     !
-!   (for no preconditioning), 1 (for diagonal preconditioning) or 2 (for       !
-!   incomplete Cholesky preconditioning)                                       !
 !------------------------------------------------------------------------------!
 !----------------------------------[Modules]-----------------------------------!
   use Matrix_Mod
@@ -20,56 +12,33 @@
   type(Matrix) :: A
   type(Matrix) :: D
   real         :: x(-NB:N), b(N)
-  integer      :: prec
 !-----------------------------------[Locals]-----------------------------------!
   integer :: i, j, k
   real    :: sum1
 !==============================================================================!
            
-  !---------------------------------! 
-  !   1) diagonal preconditioning   !
-  !---------------------------------!
-  if(prec == 1) then
-    do i=1,N
-      x(i)=b(i)/D % val(D % dia(i))
+  ! Forward substitutionn
+  do i=1,N
+    sum1=b(i)
+    do j=A % row(i),A % dia(i)-1  ! only the lower triangular
+      k = A % col(j)             
+      sum1 = sum1- A % val(j)*x(k)  
     end do
+    x(i) = sum1 * D % val(D % dia(i))         ! BUG ?
+  end do
 
-  !--------------------------------------------! 
-  !   2) incomplete cholesky preconditioning   !
-  !--------------------------------------------!
-  else if(prec == 2) then
+  do i=1,N
+    x(i) = x(i) / ( D % val(D % dia(i)) )
+  end do
 
-    ! Forward substitutionn
-    do i=1,N
-      sum1=b(i)
-      do j=A % row(i),A % dia(i)-1  ! only the lower triangular
-        k = A % col(j)             
-        sum1 = sum1- A % val(j)*x(k)  
-      end do
-      x(i) = sum1 * D % val(D % dia(i))         ! BUG ?
+  ! Backward substitution
+  do i=N,1,-1
+    sum1=x(i)
+    do j = A % dia(i)+1, A % row(i+1)-1 ! upper triangular 
+      k = A % col(j)                  
+      sum1 = sum1 - A % val(j)*x(k)      
     end do
-
-    do i=1,N
-      x(i) = x(i) / ( D % val(D % dia(i)) )
-    end do
-
-    ! Backward substitution
-    do i=N,1,-1
-      sum1=x(i)
-      do j = A % dia(i)+1, A % row(i+1)-1 ! upper triangular 
-        k = A % col(j)                  
-        sum1 = sum1 - A % val(j)*x(k)      
-      end do
-      x(i) = sum1* D % val(D % dia(i))               ! BUG ?
-    end do
-
-  !---------------------------!
-  !   .) no preconditioning   !
-  !---------------------------!
-  else
-    do i=1,N
-      x(i)=b(i)
-    end do
-  end if
+    x(i) = sum1* D % val(D % dia(i))               ! BUG ?
+  end do
 
   end subroutine Prec_Solve
