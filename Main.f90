a72f6ad6d93aedca15faaabad51153cc458d6e53
Niceno <bojan@niceno.ch>
2017-10-20 10:06:25 +0200

    Added more solvers (LDLT) to check.

--- Main.f90
+++ Main.f90
@@ -16,10 +16,12 @@
   write(*,*) '# 1 - Compressed matrices'
   write(*,*) '# 2 - Gaussian elimination'
   write(*,*) '# 3 - Cholesky factorization'
-  write(*,*) '# 4 - Incomplete Cholesky Solver'
-  write(*,*) '# 5 - Incomplete Cholesky Solver with level 1 fill-in'
-  write(*,*) '# 6 - Incomplete Cholesky Solver with level 2 fill-in'
-  write(*,*) '# 7 - Incomplete Cholesky Solver with level 3 fill-in'
+  write(*,*) '# 4 - LDL^T Solver'
+  write(*,*) '# 5 - Incomplete Cholesky Solver'
+  write(*,*) '# 6 - Incomplete Cholesky Solver with level 1 fill-in'
+  write(*,*) '# 7 - Incomplete Cholesky Solver with level 2 fill-in'
+  write(*,*) '# 8 - Incomplete Cholesky Solver with level 3 fill-in'
+  write(*,*) '# 9 - LDL^T Solver from T-FlowS' 
   write(*,*) '#----------------------------------------------------'
   read(*,*) choice
 
@@ -39,21 +41,29 @@
   else if(choice == 3) then
     call Demo_Cholesky_Solver
 
-  ! Demonstrate Incomplete Cholesky Solver
+  ! Demonstrate Cholesky Factorization
   else if(choice == 4) then
+    call Demo_LDLT_Solver
+
+  ! Demonstrate Incomplete Cholesky Solver
+  else if(choice == 5) then
     call Demo_Incomplete_Cholesky_Solver(0)
 
   ! Demonstrate Incomplete Cholesky Solver with level 1 fill in
-  else if(choice == 5) then
+  else if(choice == 6) then
     call Demo_Incomplete_Cholesky_Solver(1)
 
   ! Demonstrate Incomplete Cholesky Solver with level 2 fill in
-  else if(choice == 6) then
+  else if(choice == 7) then
     call Demo_Incomplete_Cholesky_Solver(2)
 
   ! Demonstrate Incomplete Cholesky Solver with level 3 fill in
-  else if(choice == 7) then
+  else if(choice == 8) then
     call Demo_Incomplete_Cholesky_Solver(3)
+
+  ! Demonstrate LDL^T Solver from T-FlowS
+  else if(choice == 9) then
+    call Demo_LDLT_Solver_From_Tflows
 
   end if
 
