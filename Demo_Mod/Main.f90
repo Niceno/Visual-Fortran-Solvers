e7bf4397bec39c9404ad8f5db5ebf0e78418e739
Niceno <bojan.niceno.scientist@gmail.com>
2020-09-20 16:59:50 +0200

    Shortened the syntax.

--- Demo_Mod/Main.f90
+++ Demo_Mod/Main.f90
@@ -51,25 +51,25 @@
   end if
 
   ! Open the main menu
-1 write(*,*) '#=========================================================='
-  write(*,*) '# Select a case to demonstrate'
-  write(*,*) '#----------------------------------------------------------'
-  write(*,*) '#  0 - Exit'
-  write(*,*) '# - - - - - - - - - - - - - - - - - - - - - - - - - - - - -'
-  write(*,*) '#  1 - Gaussian elimination'
-  write(*,*) '#  2 - Cholesky solver'
-  write(*,*) '#  3 - LDL^T solver'
-  write(*,*) '# - - - - - - - - - - - - - - - - - - - - - - - - - - - - -'
-  write(*,*) '#  4 - Incomplete Cholesky solver'
-  write(*,*) '#  5 - Incomplete LDL^T solver'
-  write(*,*) '#  6 - Bare-bones LDL^T solver from T-Flows'
-  write(*,*) '# - - - - - - - - - - - - - - - - - - - - - - - - - - - - -'
-  write(*,*) '#  7 - CG solver'
-  write(*,*) '#  8 - Preconditioned CG solver'
-  write(*,*) '# - - - - - - - - - - - - - - - - - - - - - - - - - - - - -'
-  write(*,*) '#  9 - Compressed matrices'
-  write(*,*) '# 10 - Preconditioning matrix'
-  write(*,*) '# - - - - - - - - - - - - - - - - - - - - - - - - - - - - -'
+1 print *, '#=========================================================='
+  print *, '# Select a case to demonstrate'
+  print *, '#----------------------------------------------------------'
+  print *, '#  0 - Exit'
+  print *, '# - - - - - - - - - - - - - - - - - - - - - - - - - - - - -'
+  print *, '#  1 - Gaussian elimination'
+  print *, '#  2 - Cholesky solver'
+  print *, '#  3 - LDL^T solver'
+  print *, '# - - - - - - - - - - - - - - - - - - - - - - - - - - - - -'
+  print *, '#  4 - Incomplete Cholesky solver'
+  print *, '#  5 - Incomplete LDL^T solver'
+  print *, '#  6 - Bare-bones LDL^T solver from T-Flows'
+  print *, '# - - - - - - - - - - - - - - - - - - - - - - - - - - - - -'
+  print *, '#  7 - CG solver'
+  print *, '#  8 - Preconditioned CG solver'
+  print *, '# - - - - - - - - - - - - - - - - - - - - - - - - - - - - -'
+  print *, '#  9 - Compressed matrices'
+  print *, '# 10 - Preconditioning matrix'
+  print *, '# - - - - - - - - - - - - - - - - - - - - - - - - - - - - -'
   write(*,'(a46,3i4)') '# 11 - Change grid resolution, currently at: ',  &
                        grid % nx, grid % ny, grid % nz
   write(*,'(a46,1i4)') '# 12 - Change fill-in level, currently at:   ',  &
@@ -78,8 +78,8 @@
                        n_iter
   write(*,'(a46,1es13.4)') '# 14 - Change target residual, currently at: ',  &
                        res
-  write(*,*) '#----------------------------------------------------------'
-  read(*,*) choice
+  print *, '#----------------------------------------------------------'
+  read *, choice
 
   if(choice ==  0) return
 
@@ -98,20 +98,20 @@
   if(choice == 10) call Demo_Mod_Fill_In(f_in, grid)
 
   if(choice == 11) then
-    write(*,*) '# Enter the desired resolution: '
-    read(*,*) grid % nx, grid % ny, grid % nz
+    print *, '# Enter the desired resolution: '
+    read *, grid % nx, grid % ny, grid % nz
   end if
   if(choice == 12) then
-    write(*,*) '# Enter the desired fill-in level: '
-    read(*,*) f_in
+    print *, '# Enter the desired fill-in level: '
+    read *, f_in
   end if
   if(choice == 13) then
-    write(*,*) '# Enter the desired number of iterations: '
-    read(*,*) n_iter
+    print *, '# Enter the desired number of iterations: '
+    read *, n_iter
   end if
   if(choice == 14) then
-    write(*,*) '# Enter the desired target residual: '
-    read(*,*) res
+    print *, '# Enter the desired target residual: '
+    read *, res
   end if
 
   goto 1
