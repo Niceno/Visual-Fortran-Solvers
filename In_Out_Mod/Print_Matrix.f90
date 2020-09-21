4da4aa9c3a157aca01a6d477c36b8e7ddec4acc5
Niceno <bojan.niceno.scientist@gmail.com>
2020-09-21 12:52:19 +0200

    Prints matrices in more colors.

--- In_Out_Mod/Print_Matrix.f90
+++ In_Out_Mod/Print_Matrix.f90
@@ -10,27 +10,32 @@
 !-----------------------------------[Locals]-----------------------------------!
   integer      :: row, col  ! row used to be "i", col used to be "j"
   character(2) :: item
+  real         :: max_val
 !==============================================================================!
 
   if(size(full,1) > 64) return
 
   print *, message
 
+  ! Find maximum entry in the system matrix
+  max_val = 0
+  max_val = maxval(full)
+
+  ! Print the matrix
   do row = 1, size(full, 1)
     do col = 1, size(full, 2)
       write(item(1:2), '(a2)') '  '
 
-      ! Diagonal terms in red
-      if(row .eq. col) then
+      if( abs(full(row, col)) > 0.5 * max_val) then
         call Foul_Mod_Write(item, 'red background_red', forward='no')
-
-      ! Off-diagonal terms
+      else if( abs(full(row, col)) > 0.01 * max_val) then
+        call Foul_Mod_Write(item, 'yellow background_yellow', forward='no')
+      else if( abs(full(row, col)) > 0.001 * max_val) then
+        call Foul_Mod_Write(item, 'green background_green', forward='no')
+      else if( abs(full(row, col)) > TINY * max_val) then
+        call Foul_Mod_Write(item, 'blue background_blue', forward='no')
       else
-        if(abs(full(row,col)) < TINY) then
-          call Foul_Mod_Write(item, 'blue background_blue', forward='no')
-        else
-          call Foul_Mod_Write(item, 'green background_green', forward='no')
-        end if
+        call Foul_Mod_Write(item, 'magenta background_magenta', forward='no')
       end if
 
     end do
--- In_Out_Mod/Print_Matrix_Compressed.f90
+++ In_Out_Mod/Print_Matrix_Compressed.f90
@@ -11,11 +11,16 @@
   integer      :: row, col, pos  ! row used to be "i", col used to be "j"
   logical      :: found
   character(2) :: item
+  real         :: max_val
 !==============================================================================!
 
   if(sparse % n > 64) return
 
   print *, message
+
+  ! Find maximum entry in the system matrix
+  max_val = 0
+  max_val = maxval(sparse % val)
 
   do row = 1, sparse % n
     do col = 1, sparse % n
@@ -29,13 +34,16 @@
 
         if( sparse % col(pos) == col ) then
 
-          ! Diagonal terms in red
-          if(row .eq. col) then
+          if( abs(sparse % val(pos)) > 0.5 * max_val) then
             call Foul_Mod_Write(item, 'red background_red', forward='no')
-
-          ! Off-diagonal terms
-          else
+          else if( abs(sparse % val(pos)) > 0.01 * max_val) then
+            call Foul_Mod_Write(item, 'yellow background_yellow', forward='no')
+          else if( abs(sparse % val(pos)) > 0.001 * max_val) then
             call Foul_Mod_Write(item, 'green background_green', forward='no')
+          else if( abs(sparse % val(pos)) > TINY * max_val) then
+            call Foul_Mod_Write(item, 'blue background_blue', forward='no')
+          else
+            call Foul_Mod_Write(item, 'magenta background_magenta', forward='no')
           end if
 
           found = .true.
