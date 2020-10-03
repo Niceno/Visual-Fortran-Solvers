f5596babbe34118a737a7821b0e3c594311588ce
Niceno <bojan.niceno.scientist@gmail.com>
2020-10-03 22:32:54 +0200

    Added a couple of OpenACC directives.

--- Solvers_Mod/Cg.f90
+++ Solvers_Mod/Cg.f90
@@ -8,7 +8,7 @@
   real            :: res
 !-----------------------------------[Locals]-----------------------------------!
   integer :: n  ! number of unknowns
-  integer :: i
+  integer :: i, iter
   real    :: alpha, beta, rho_old, rho, pap
   real    :: time_ps, time_pe, time_ss, time_se
 !==============================================================================!
@@ -56,7 +56,7 @@
   !                               !
   !-------------------------------!
   call Cpu_Time(time_ss)
-  do i = 1, n_iter
+  do iter = 1, n_iter
 
     !------------!
     !   p = Ap   !
@@ -73,7 +73,10 @@
     !   x = x + alfa p    !
     !   r = r - alfa Ap   !
     !---------------------!
-    x(1:n) = x(1:n) + alpha * p(1:n)
+    !$acc parallel loop
+    do i = 1, n
+      x(i) = x(i) + alpha * p(i)
+    end do
     r(1:n) = r(1:n) - alpha * ap(1:n)
 
     !------------------!
@@ -82,14 +85,18 @@
     rho_old = rho
     call Lin_Alg_Mod_Vector_Vector_Dot_Product(rho, r, r)
 
-    print '(a,i3,a,1es10.4)', ' #', i, '; rho = ', sqrt(rho)
+    print '(a,i3,a,1es10.4)', ' #', iter, '; rho = ', sqrt(rho)
     if(sqrt(rho) < res) goto 1
 
     !---------------------------------!
     !   p = r + (rho / rho_old) * p   !
     !---------------------------------!
     beta = rho / max(rho_old, 1.0e-12)
-    p(1:n) = r(1:n) + beta * p(1:n)
+
+    !$acc parallel loop
+    do i = 1, n
+      p(i) = r(i) + beta * p(i)
+    end do
   end do
 
 1 continue
