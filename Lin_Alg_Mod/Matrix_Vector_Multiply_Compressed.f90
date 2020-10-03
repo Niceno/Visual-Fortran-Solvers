dd2bd5733b323e488aca46d692fc21542415c470
Niceno <bojan.niceno.scientist@gmail.com>
2020-10-03 22:32:02 +0200

    Inserted the first OpenACC directive which worked! (No profiling whatsoever at this stage.)

--- Lin_Alg_Mod/Matrix_Vector_Multiply_Compressed.f90
+++ Lin_Alg_Mod/Matrix_Vector_Multiply_Compressed.f90
@@ -10,16 +10,23 @@
   real, dimension(:) :: x
 !-----------------------------------[Locals]-----------------------------------!
   integer :: i, j, i_j, n
+  real    :: y_i_sum
 !==============================================================================!
 
-  n = c % n  ! some checks would be possible
+  n  = c % n  ! some checks would be possible
 
+  !$acc  parallel loop                                      &
+  !$acc& copyin (c, c % row(:), c % col(:), c % val(:), x)  &
+  !$acc& copyout(y)                                         &
+  !$acc& gang worker vector_length(32) num_workers(32)
   do i = 1, n
-    y(i) = 0.0
+    y_i_sum = 0.0
+    !$acc loop vector reduction(+:y_i_sum)
     do i_j = c % row(i), c % row(i+1) - 1
       j = c % col(i_j)
-      y(i) = y(i) + c % val(i_j) * x(j)
+      y_i_sum = y_i_sum + c % val(i_j) * x(j)
     end do
+    y(i) = y_i_sum
   end do
 
   end subroutine
