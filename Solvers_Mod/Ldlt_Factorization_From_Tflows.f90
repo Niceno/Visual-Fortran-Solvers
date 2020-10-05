cf537dcff576dbe9ea38df5c83e0132413292aa3
Niceno <bojan.niceno.scientist@gmail.com>
2020-10-05 17:42:57 +0200

    Changed the order of arguments for uniformity.

--- Solvers_Mod/Ldlt_Factorization_From_Tflows.f90
+++ Solvers_Mod/Ldlt_Factorization_From_Tflows.f90
@@ -1,15 +1,15 @@
 !==============================================================================!
-  subroutine Solvers_Mod_Ldlt_Factorization_From_Tflows(a, d)
+  subroutine Solvers_Mod_Ldlt_Factorization_From_Tflows(f, a)
 !------------------------------------------------------------------------------!
-!   Forms preconditioning matrix "d" from provided matrix "a".                 !
+!   Forms preconditioning matrix "f" from provided matrix "a".                 !
 !                                                                              !
 !   Called by:                                                                 !
 !   - Solvers_Mod_Incomplete_Ldlt_From_Tflows                                  !
 !------------------------------------------------------------------------------!
   implicit none
 !---------------------------------[Arguments]----------------------------------!
+  type(Sparse_Type) :: f
   type(Sparse_Type) :: a
-  type(Sparse_Type) :: d
 !-----------------------------------[Locals]-----------------------------------!
   real     :: sum1
   integer  :: n, i, j, k
@@ -21,11 +21,11 @@
     sum1 = a % val(a % dia(i))       ! take diaginal entry
     do j = a % row(i), a % dia(i)-1  ! only lower traingular
       k = a % col(j)
-      sum1 = sum1 - d % val(d % dia(k)) * a % val(j) * a % val(j)
+      sum1 = sum1 - f % val(f % dia(k)) * a % val(j) * a % val(j)
     end do
 
     ! This is only the diagonal from LDL decomposition
-    d % val(d % dia(i)) = 1.0 / sum1
+    f % val(f % dia(i)) = 1.0 / sum1
   end do
 
   end subroutine
