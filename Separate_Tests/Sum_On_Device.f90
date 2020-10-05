646b69f2338d0d9da326718e7e127bba2838632e
Niceno <bojan.niceno.scientist@gmail.com>
2020-10-05 11:25:52 +0200

    Added directory for separate tests.

---
+++ Separate_Tests/Sum_On_Device.f90
@@ -0,0 +1,31 @@
+  program Dot_Product
+
+  integer, parameter :: n = 1000 ! vector size
+  integer, parameter :: m =   15 ! iterations
+  real, dimension(n) :: x, y
+
+  ! Initialize on the host
+  do i = 1, n
+    x(i) = i * 0.0001;
+    y(i) = i * 0.0002;
+  end do
+
+  prod = 0
+  !$acc enter data copyin(x, y, prod, n)
+
+  do j = 1, m
+
+    !$acc  parallel loop reduction(+:prod)  &
+    !$acc& present(x, y, prod, n)
+    do i = 1, n
+      prod = prod + x(i) * y(i)
+    end do
+
+  end do
+
+  !$acc update self(prod)
+  !$acc exit data delete(x, y)
+
+  print *, 'dot product is: ', prod
+
+  end program
