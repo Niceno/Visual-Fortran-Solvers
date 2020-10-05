530c41cfd204fff3b77e378cebf9cb9fc3739827
Niceno <bojan.niceno.scientist@gmail.com>
2020-10-05 21:30:23 +0200

    Syntax more readable now.

--- Solvers_Mod/Ldlt_Solution_From_Tflows.f90
+++ Solvers_Mod/Ldlt_Solution_From_Tflows.f90
@@ -19,10 +19,10 @@
 
   ! Forward substitutionn
   do i = 1, n
-    sum1=b(i)
-    do j = a % row(i), a % dia(i)-1  ! only the lower triangular
+    sum1 = b(i)
+    do j = a % row(i), a % dia(i) - 1  ! only the lower triangular
       k = a % col(j)
-      sum1 = sum1- a % val(j)*x(k)
+      sum1 = sum1 - a % val(j) * x(k)
     end do
     x(i) = sum1 * d % val(d % dia(i))         ! BUG ?
   end do
@@ -34,9 +34,9 @@
   ! Backward substitution
   do i = n, 1, -1
     sum1=x(i)
-    do j = a % dia(i)+1, a % row(i+1)-1 ! upper triangular
+    do j = a % dia(i) + 1, a % row(i+1) - 1 ! upper triangular
       k = a % col(j)
-      sum1 = sum1 - a % val(j)*x(k)
+      sum1 = sum1 - a % val(j) * x(k)
     end do
     x(i) = sum1 * d % val(d % dia(i))               ! BUG ?
   end do
