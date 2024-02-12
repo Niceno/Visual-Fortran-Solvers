d59abe77d0e82fa4075721f263db94a4090563e5
Niceno <bojan.niceno.scientist@gmail.com>
2024-02-12 22:02:51 +0100

    Plots it and makes it operate on L part only
    On branch master
    
    modified:   Solvers_Mod/Dense/Ldlt_Factorization.f90

--- Solvers_Mod/Dense/Ldlt_Factorization.f90
+++ Solvers_Mod/Dense/Ldlt_Factorization.f90
@@ -15,29 +15,40 @@
   type(Dense_Type) :: LDL  !! factorized matrix (three in one, really)
   type(Dense_Type) :: A    !! original matrix
 !-----------------------------------[Locals]-----------------------------------!
-  integer :: i, k, m
+  integer :: i, k, s, n, bw
   real    :: sum
 !==============================================================================!
 
   print *, '# Factorizing square (full) matrix with LDL'' method'
 
+  ! Take some aliases
+  n  = A % n
+  bw = A % bw
+
   ! Initialize the values
   LDL % val(:,:) = 0.0
 
   ! Perform the factorization
-  do k = 1, A % n
-    sum = A % val(k,k)
-    do m = max(1, k - A % bw), k - 1
-      sum = sum - LDL % val(k,m) * LDL % val(k,m) * LDL % val(m,m)
+  do k = 1, n
+
+    ! Work out the diagonal D
+    sum = 0.0
+    do s = max(1, k - bw), k - 1
+      sum = sum + LDL % val(k,s)**2 * LDL % val(s,s)
+      call IO % Plot_Dense("factorization", LDL, B=A, src1=(/k,s,GREEN/), src2=(/s,s,GREEN2/))
     end do
-    LDL % val(k,k) = sum                   ! diagonal entry, D from LDL
-    do i = k + 1, min(k + A % bw, A % n)
-      sum = A % val(i,k)
-      do m = max(1, k - A % bw), k - 1
-        sum = sum - LDL % val(m,i) * LDL % val(m,k) * LDL % val(m,m)
+    LDL % val(k,k) = A % val(k,k) - sum      ! diagonal entry, D from LDL
+    call IO % Plot_Dense("factorization", LDL, B=A, targ=(/k,k,PINK2/))
+
+    ! Work out the diagonal L
+    do i = k + 1, min(k + bw, n)
+      sum = 0.0
+      do s = max(1, k - bw, i - bw), k - 1
+        sum = sum + LDL % val(i,s) * LDL % val(k,s) * LDL % val(s,s)
+        call IO % Plot_Dense("factorization", LDL, B=A, src1=(/i,s,GREEN/), src2=(/k,s,GREEN2/), src3=(/s,s,GREEN4/))
       end do
-      LDL % val(k,i) = sum / LDL % val(k,k)  ! upper triangle, the L' here
-      LDL % val(i,k) = sum / LDL % val(k,k)  ! lower triangle, the L part
+      LDL % val(i,k) = (A % val(i,k) - sum) / LDL % val(k,k)
+      call IO % Plot_Dense("factorization", LDL, B=A, targ=(/i,k,PINK2/))
     end do
   end do
 
