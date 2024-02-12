3756ef88a79b10808ce5f24fda40cea62ca8231e
Niceno <bojan.niceno.scientist@gmail.com>
2024-02-12 22:01:27 +0100

    Made it operate on L part of the matrix only
    It is symmetric anyhoo
    
    On branch master
    
    modified:   Solvers_Mod/Dense/Ldlt_Solution.f90

--- Solvers_Mod/Dense/Ldlt_Solution.f90
+++ Solvers_Mod/Dense/Ldlt_Solution.f90
@@ -1,5 +1,5 @@
 !==============================================================================!
-  subroutine Solvers_Mod_Dense_Ldlt_Solution(x, F, b)
+  subroutine Solvers_Mod_Dense_Ldlt_Solution(x, L, b)
 !------------------------------------------------------------------------------!
 !>  Solves system based on LDL' decomposition.
 !------------------------------------------------------------------------------!
@@ -9,35 +9,35 @@
   implicit none
 !---------------------------------[Arguments]----------------------------------!
   real, dimension(:) :: x  !! solution vector
-  type(Dense_Type)   :: F  !! factorized matrix
+  type(Dense_Type)   :: L  !! factorized matrix
   real, dimension(:) :: b  !! right hand side vector
 !-----------------------------------[Locals]-----------------------------------!
   integer :: i, j, n, bw
   real    :: sum
 !==============================================================================!
 
-  n  = F % n  ! some checks would be possible
-  bw = F % bw
+  n  = L % n  ! some checks would be possible
+  bw = L % bw
 
-  ! Forward substitutions
+  ! Forward substitution (j < i =--> L)
   do i = 1, n
     sum = b(i)
     do j = max(1, i - bw), i-1
-      sum = sum - F % val(i,j) * x(j)  ! straightforward for compressed row format
+      sum = sum - L % val(i,j) * x(j)  ! straightforward for compressed row format
     end do
     x(i) = sum
   end do
 
   ! Treat the diagonal term
   do i = 1, n
-    x(i) = x(i) / F % val(i,i)
+    x(i) = x(i) / L % val(i,i)
   end do
 
-  ! Backward substitution
+  ! Backward substitution (j > i =--> U; use L but transposed (see j,i))
   do i = n, 1, -1
     sum = x(i)
     do j = i+1, min(i + bw, n)
-      sum = sum - F % val(i,j) * x(j)  ! straighforward for compressed row format
+      sum = sum - L % val(j,i) * x(j)  ! straighforward for compressed row format
     end do
     x(i) = sum
   end do
