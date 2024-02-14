d7f29a6992e4fd65113b0f472e13829efeece109
Niceno <bojan.niceno.scientist@gmail.com>
2024-02-14 20:45:37 +0100

    Fixed Assert_Mod, it didn't work at all.
    What is still missing is the ability to dissengage
    assertions if one pleases to do so.
    
    On branch master
    
    modified:   Assert.h90
    modified:   Assert_Mod/Handle_Assert.f90

--- Assert_Mod/Handle_Assert.f90
+++ Assert_Mod/Handle_Assert.f90
@@ -5,23 +5,6 @@
 !>  an assertion fails. It's is called exclusivelly from the Assert macro
 !>  defined in Assert.h90.
 !------------------------------------------------------------------------------!
-!   Functionality:                                                             !
-!                                                                              !
-!   * Sequential Run Handling:                                                 !
-!     - If the code is running in a sequential (non-parallel) environment      !
-!       (checked by Sequential_Run()), it checks if the assertion failed       !
-!       (fail is true).                                                        !
-!     - In case of a failure, it formats and prints an error message including !
-!       the assertion condition (text), file name, and line number.            !
-!     - After printing the error message, it calls Global % End_Parallel and   !
-!       then stops the program execution using stop.                           !
-!   * Parallel Run Handling:                                                   !
-!     - If the code is running in a parallel environment, the subroutine       !
-!       additionally includes the processor number in the error message        !
-!       (obtained by This_Proc()).                                             !
-!     - The rest of the handling is similar to the sequential run, with the    !
-!       program terminating upon a failed assertion.                           !
-!------------------------------------------------------------------------------!
   implicit none
 !---------------------------------[Arguments]----------------------------------!
   logical      :: fail  !! flag indicating whether the assertion has failed
@@ -32,32 +15,13 @@
   character(16) :: numb, proc
 !==============================================================================!
 
-  ! Sequential run
-  if(Sequential_Run()) then
-    if(fail) then
-      write(numb, '(i16)') line
-      print '(7a)', '  Assert(',         text,                 &
-                    ') failed in file ', file,                 &
-                    ' at line ',         trim(adjustl(numb)),  &
-                    '.'
-      call Global % End_Parallel
-      stop
-    end if
-
-  ! Parallel run
-  else
-    if(fail) then
-      write(numb, '(i16)') line
-      write(proc, '(i16)') This_Proc()
-      print '(9a)', '  Assert(',         text,                 &
-                    ') failed in file ', file,                 &
-                    ' at line ',         trim(adjustl(numb)),  &
-                    ' in processor ',    trim(adjustl(proc)),  &
-                    '.'
-      call Global % End_Parallel
-      stop
-    end if
-
+  if(fail) then
+    write(numb, '(i16)') line
+    print '(7a)', '  Assert(',         text,                 &
+                  ') failed in file ', file,                 &
+                  ' at line ',         trim(adjustl(numb)),  &
+                  '.'
+    stop
   end if
 
   end subroutine
